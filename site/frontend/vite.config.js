9da2c56cbde4c586b26a390092c0c0c07f1578bc
ApurvChaturOfficial <apurvchaturoffiial@gmail.com>
2024-01-10 11:55:49 +0530

    Jiwan

--- site/frontend/vite.config.js
+++ site/frontend/vite.config.js
@@ -8,16 +8,26 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      manifest:  {
-        "name": "Andromeda Organisation",
-        "icons": [
+      manifest: {
+        "registerType": 'autoUpdate',
+        "name":"Andromeda Organisation",
+        "short_name":"Andromeda",
+        "display": "standalone",
+        "background_color": "#22092C",
+        "theme_color": "#22092C",
+        "icons":[
           {
             src: "Business/Andromeda/favicon_io/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
-          }
+          },
+          {
+            src: "Business/Andromeda/favicon_io/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
         ],
-      } 
+      }
     })
   ],
  
